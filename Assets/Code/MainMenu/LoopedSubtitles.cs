b2ebb79e0757a711cdc91358d2069f98c7857907
Brajanowski <brajanbartoszewicz@gmail.com>
2024-01-25 22:33:16 +0100

    added some lore to main menu

---
+++ Assets/Code/MainMenu/LoopedSubtitles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace MainMenu
+{
+    [Serializable]
+    public struct SubtitleEntry
+    {
+        public string Text;
+        public float Duration;
+    }
+
+    public class LoopedSubtitles : MonoBehaviour
+    {
+        [SerializeField]
+        private SubtitleEntry[] _entries;
+
+        [SerializeField]
+        private TextMeshProUGUI _text;
+
+        private float _timer;
+        private int _currentSubtitle;
+
+        private void OnEnable()
+        {
+            SetSubtitle(0);
+        }
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0.0f)
+            {
+                SetSubtitle((_currentSubtitle + 1) % _entries.Length);
+            }
+        }
+
+        private void SetSubtitle(int index)
+        {
+            _currentSubtitle = index;
+            _timer = _entries[index].Duration;
+            _text.text = _entries[index].Text;
+        }
+    }
+}
